d48ebe095e66e821b0b331750ef90118de1252f8
yemihunter <yemihunter@gmail.com>
2022-11-16 00:37:05 +0100

    Add mobile nav popup with home and signout functionality

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,5 +1,8 @@
 <template>
   <header>
+    <!-- sign up successful component -->
+    <signOut v-show="signOutDisplay"></signOut>
+
     <nav class="nav-small">
       <a href="#"
         ><img
@@ -8,7 +11,7 @@
           alt="back arrow"
         />
       </a>
-      <a class="collasped-meun" href="#"
+      <a class="collasped-meun" href="#" @click="toggleMobilePopOpen"
         ><img
           class="collasped-meun"
           src="../assets/icons/bars-solid.svg"
@@ -23,7 +26,7 @@
         <h1 class="logo-text">MedBookly</h1>
       </a>
       <ul class="nav-list">
-        <li class="nav-item"><a href="#">Home</a></li>
+        <router-link class="nav-item" to="/"><a href="#">Home</a></router-link>
         <li class="nav-item"><a href="#">Booking</a></li>
         <li class="nav-item"><a href="#">Settings</a></li>
       </ul>
@@ -31,10 +34,11 @@
         <span class="search-input">
           <img
             class="search-icon"
-            src="../assets/icons/search-icon.svg"
+            src="@/assets/icons/search-icon.svg"
             alt="search_icon"
+            @click="openSearch"
           />
-          <input class="nav-input" type="text" />
+          <input :class="searchInput" type="text" />
         </span>
         <div class="profile-container" @click="handleToggle">
           <span class="profile-avatar"
@@ -50,30 +54,82 @@
                 class=""
                 src="../assets/icons/more-arrow.svg"
                 alt="search_icon"
+                @click="openSearch"
               />
             </span>
           </span>
         </div>
-        <div v-show="profileDrop" class="profile-drop">
+        <div v-show="profileDropDown" class="profile-drop">
           <a href="#">View Profile</a>
-          <a href="#">Logout</a>
+          <a href="#" @click="showSignOut">Logout</a>
         </div>
       </div>
     </nav>
+
+    <!-- mobile nav popup -->
+    <div class="mobile-pop" v-show="showMobilePop">
+      <div class="popContainer">
+        <div class="cancleicon">
+          <a href=""><img src="../assets/icons/Search.svg" alt="" /></a>
+          <a href=""
+            ><img
+              src="../assets/icons/Closecancel.svg"
+              class="cancle"
+              alt=""
+              @click="toggleMobilePopClose"
+          /></a>
+        </div>
+
+        <nav>
+          <ul>
+            <router-link class="mobileList" to="/"
+              ><a href="#">Home</a></router-link
+            >
+            <li class="mobileList"><a href="#">Booking</a></li>
+            <li class="mobileList"><a href="#">Profile</a></li>
+            <li class="mobileList"><a href="#" class="stn">Settings</a></li>
+          </ul>
+        </nav>
+
+        <div class="logout">
+          <a href="#" @click="showSignOut">Logout</a>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
+import signOut from "@/components/signOut.vue";
 export default {
   name: "navBar",
   data() {
     return {
-      profileDrop: false,
+      profileDropDown: false,
+      signOutDisplay: false,
+      searchInput: "search-close search-anim",
+      showMobilePop: false,
     };
+  },
+  components: {
+    signOut,
   },
   methods: {
     handleToggle() {
-      this.profileDrop = !this.profileDrop;
+      this.profileDropDown = !this.profileDropDown;
+    },
+    showSignOut() {
+      this.signOutDisplay = !this.signOutDisplay;
+      this.profileDropDown = false;
+    },
+    openSearch() {
+      this.searchInput = "nav-input search-anim";
+    },
+    toggleMobilePopOpen() {
+      this.showMobilePop = true;
+    },
+    toggleMobilePopClose() {
+      this.showMobilePop = false;
     },
   },
 };
@@ -81,6 +137,22 @@ export default {
 
 <style scoped>
 /* For Mobile Devices */
+
+/* collapse search bar */
+.search-close {
+  display: none;
+}
+.search-anim {
+  transition: all ease-in 10s;
+}
+
+.nav-input {
+  border-radius: 10px;
+  padding-right: 12px;
+  color: var(--color-nav-item);
+  width: 170px;
+  max-width: 170px;
+}
 
 .nav-bar {
   display: none;
@@ -107,8 +179,83 @@ export default {
   height: 20.3px;
 }
 
+/* mobile nav side popup */
+
+.mobile-pop {
+  background: rgba(0, 0, 0, 0.537);
+  position: absolute;
+  z-index: 900;
+  width: 100vw;
+  height: 100vh;
+}
+
+.popContainer {
+  position: absolute;
+  left: 0;
+  width: 50vw;
+  height: 100vh;
+  background: white;
+  left: 50%;
+}
+
+.cancleicon {
+  padding: 20px;
+  width: 100%;
+  /* height: 30px; */
+  border-bottom: 0.5px solid #bfbfbf;
+}
+
+.cancle {
+  position: absolute;
+  right: 20px;
+}
+
+.cancleicon img {
+  width: 22px;
+}
+
+nav ul li {
+  list-style-type: none;
+}
+
+.mobileList a {
+  text-decoration: none;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding-top: 16px;
+  padding-left: 25px;
+  border-bottom: 0.5px solid #bfbfbf;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #262626;
+}
+
+a.stn {
+  color: #246bfd;
+}
+
+.logout {
+  position: absolute;
+  bottom: 2%;
+  right: 13px;
+  margin-right: 5vw;
+}
+
+.logout a {
+  text-decoration: none;
+  color: red;
+  font-size: 13px;
+}
+
 /* For Desktop >= 800px */
 @media screen and (min-width: 800px) {
+  .mobile-pop {
+    display: none;
+  }
   .nav-small {
     display: none;
   }
@@ -141,6 +288,31 @@ export default {
     font-weight: 700;
   }
 
+  .nav-input {
+    border-radius: 10px;
+    padding-right: 12px;
+    color: var(--color-nav-item);
+    width: 170px;
+    max-width: 170px;
+  }
+
+  .search-input {
+    display: flex;
+    border: 1px solid var(--color-placeholder);
+    border-radius: 10px;
+    color: var(--color-placeholder);
+    outline: 0;
+  }
+
+  .search-input .nav-input {
+    border: 0;
+    flex-grow: 1;
+    outline: 0;
+  }
+
+  .search-input img {
+    margin: 11.29px;
+  }
   .nav-bar a {
     text-decoration: none;
     transition: color 0.5s;
@@ -156,6 +328,10 @@ export default {
   }
 
   .nav-item:hover a {
+    color: var(--color-primary);
+  }
+
+  .nav-item:active a {
     color: var(--color-primary);
   }
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,28 @@
 <template>
+  <navBar class="nav" />
   <div class="home">
     <h1>Home Page</h1>
   </div>
 </template>
 
 <script>
+import navBar from "@/components/navBar.vue";
 export default {
-
   components: {
-  }
-}
+    navBar,
+  },
+};
 </script>
 
 <style scoped>
-  .home {
-    display: grid;
-    place-items: center;
-    height: 100vh;
-    width: 100%;
-  }
+/* .nav {
+  position: fixed;
+  top: 0;
+} */
+.home {
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+}
 </style>
